858bfd2fde2b56b218abdd0a2eea1c34e203d88d
Eric SHI <longwosion@gmail.com>
2013-09-13 18:36:40 +0800

    delete files in workflow folder

--- redbreast/workflow/core/__init__.py
+++
@@ -1,3 +0,0 @@
-from workflow import Workflow
-from task import Task
-from exception import WorkflowException
--- redbreast/workflow/core/exception.py
+++
@@ -1,6 +0,0 @@
-
-
-class WorkflowException(Exception):
-    def __init__(self, sender, error):
-        Exception.__init__(self, '%s: %s' % (sender.name, error))
-        self.sender = sender # Points to the Task that generated the exception.
