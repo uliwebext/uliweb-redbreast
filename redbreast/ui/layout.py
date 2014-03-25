cc5e07ceba36f2efda663774cc2fde347e9d76c9
Eric SHI <longwosion@gmail.com>
2014-03-25 13:59:47 +0800

    add param for controlling trans list

--- redbreast/ui/layout.py
+++ redbreast/ui/layout.py
@@ -5,7 +5,7 @@ class WFBasicLayout(object):
     BEFORE_HTML = ""
     AFTER_HTML = ""
 
-    def __init__(self, items):
+    def __init__(self, items, **kwargs):
         self._items = items
 
     def before_html(self):
@@ -51,8 +51,12 @@ class WFTransBoxLayout(WFTransListLayout):
     BEFORE_HTML = "<div class='wf-trans-list box-style'>"
     AFTER_HTML = "</div>"
 
+    def __init__(self, items, **kwargs):
+        super(WFTransBoxLayout, self).__init__(items, **kwargs)
+        self.show_null_message = kwargs.get("show_null_message", True)
+
     def one(self, data):
-        if not data['message']:
+        if not data['message'] and self.show_null_message:
             data['message'] = u"<i>未填写</i>"
 
         if data['from_task'] and data['to_task']:
@@ -62,15 +66,16 @@ class WFTransBoxLayout(WFTransListLayout):
         elif data['from_task']:
             operation = u" 在活动<span class='task'>【%(from_task)s】</span>执行办结操作，"
 
-        text = [
-            u"<div class='item'>",
-                u"<div class='order'>%(order)s</div>",
-                u"<div class='info'>",
-                    u"<span class='create_user'>%(created_user)s</span> 于 <span class='create_date'>%(created_date)s</span>",
-                    operation,
-                u"</div>",
-                u"<div class='trans_message'>流转意见为： %(message)s </div>"
-            u"</div>",
-        ]
+        text = []
+        text.append(u"<div class='item'>")
+        text.append(u"<div class='order'>%(order)s</div>")
+        text.append(u"<div class='info'>")
+        text.append(u"<span class='create_user'>%(created_user)s</span> 于 <span class='create_date'>%(created_date)s</span>")
+        text.append(operation)
+        text.append("</div>")
+
+        if data['message']:
+            text.append(u"<div class='trans_message'>流转意见为： %(message)s </div>")
+        text.append(u"</div>")
 
         return "".join(text) % data
--- redbreast/ui/utils.py
+++ redbreast/ui/utils.py
@@ -6,7 +6,7 @@ TRANS_TEMPLATE = ""
 class WorkflowUIUtils(object):
 
     @staticmethod
-    def print_workflow_trans(workflow, layout_class=WFTransListLayout):
+    def print_workflow_trans(workflow, layout_class=WFTransListLayout, layout_class_kwargs={}):
         from uliweb.orm import get_model
         WFTrans = get_model('workflow_trans')
         if hasattr(workflow, 'id'):
@@ -15,7 +15,7 @@ class WorkflowUIUtils(object):
             cond = WFTrans.c.workflow == workflow.get_id()
 
         trans = WFTrans.filter(cond).order_by(WFTrans.c.created_date.asc())
-        layout = layout_class(trans)
+        layout = layout_class(trans, **layout_class_kwargs)
 
         return layout.html()
 
